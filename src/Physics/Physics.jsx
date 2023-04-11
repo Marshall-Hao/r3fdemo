488a50ed3e73cf4648acb71a110c2ee450641773
Marshall Chan <marshallchan666@outlook.com>
2023-04-11 22:20:41 +0800

    feat: rigid body and collider

--- src/Physics/Physics.jsx
+++ src/Physics/Physics.jsx
@@ -5,6 +5,8 @@ import {
   Debug,
   Physics,
   RigidBody,
+  CuboidCollider,
+  BallCollider,
 } from "@react-three/rapier";
 
 export default function PhysicsExperience() {
@@ -19,34 +21,80 @@ export default function PhysicsExperience() {
       <OrbitControls makeDefault />
 
       <directionalLight
+        castShadow
         position={[1, 2, 3]}
         intensity={1.5}
       />
-      <ambientLight intensity={0.5} />
+      <ambientLight castShadow intensity={0.5} />
       <Physics>
         <Debug></Debug>
-        <RigidBody>
-          <mesh position={[-2, 2, 0]}>
+        <RigidBody
+          //  * shape of the wrapper
+          colliders="ball"
+        >
+          <mesh position={[0, 4, 0]} castShadow>
             <sphereGeometry />
             <meshStandardMaterial color="orange" />
           </mesh>
         </RigidBody>
 
-        <mesh ref={cube} position={[2, 2, 0]} scale={1.5}>
-          <boxGeometry />
-          <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+        <RigidBody
+          // * fit the shape as much as possible
+          // colliders="hull"
+          // * avoid using it with dynamic body(fast moving),too fast, it will go through,
+          // colliders="trimesh"
+          colliders={false}
+        >
+          {/* // * Cubid collider itself, args half depth x,y,z */}
+          {/* <CuboidCollider
+            args={[1.5, 1.5, 0.5]}
+          ></CuboidCollider>
+          <CuboidCollider
+            args={[0.25, 1, 0.25]}
+            position={[0, 0, 1]}
+            rotation={[-Math.PI * 0.35, 0, 0]}
+          ></CuboidCollider> */}
+          {/*  // * generate the feeling of collision */}
+          <BallCollider args={[1.5]}></BallCollider>
+          <mesh
+            castShadow
+            // position={[0, 1, 0]}
+            rotation={[Math.PI * 0.5, 0, 0]}
+          >
+            <torusGeometry
+              args={[1, 0.5, 16, 32]}
+            ></torusGeometry>
+            <meshStandardMaterial color="mediumpurple"></meshStandardMaterial>
+          </mesh>
+        </RigidBody>
+        {/* <RigidBody>
+          <mesh
+            ref={cube}
+            position={[2, 2, 0]}
+            scale={1.5}
+            castShadow
+          >
+            <boxGeometry args={[3, 2, 1]} />
+            <meshStandardMaterial color="mediumpurple" />
+          </mesh>
+
+          <mesh
+            ref={cube}
+            position={[2, 2, 3]}
+            scale={1.5}
+            castShadow
+          >
+            <boxGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color="mediumpurple" />
+          </mesh>
+        </RigidBody> */}
 
         <RigidBody
           // * no go through,will stay there,but need rigid body to make collision
           type="fixed"
         >
-          <mesh
-            position-y={-1}
-            rotation-x={-Math.PI * 0.5}
-            scale={10}
-          >
-            <planeGeometry />
+          <mesh position-y={-1.25} receiveShadow>
+            <boxGeometry args={[10, 0.5, 10]} />
             <meshStandardMaterial color="greenyellow" />
           </mesh>
         </RigidBody>
