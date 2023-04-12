de47ab06e1a69e562b18f1a17a68496896b25960
Marshall Chan <marshallchan666@outlook.com>
2023-04-12 19:04:11 +0800

    feat: instanceMesh, instanceRigidBodies new api check, debug trap

--- src/Physics/Physics.jsx
+++ src/Physics/Physics.jsx
@@ -1,6 +1,11 @@
 import { useFrame } from "@react-three/fiber";
 import { OrbitControls, useGLTF } from "@react-three/drei";
-import { useRef, useState } from "react";
+import {
+  useRef,
+  useState,
+  useEffect,
+  useMemo,
+} from "react";
 import {
   Debug,
   Physics,
@@ -8,8 +13,10 @@ import {
   CuboidCollider,
   BallCollider,
   CylinderCollider,
+  InstancedRigidBodies,
 } from "@react-three/rapier";
-import { Euler, Quaternion } from "three";
+import { Euler, Quaternion, Matrix4, Vector3 } from "three";
+import { Perf } from "r3f-perf";
 
 export default function PhysicsExperience() {
   // * will import sound once.store in the state
@@ -18,9 +25,80 @@ export default function PhysicsExperience() {
   const [hitSound] = useState(
     () => new Audio("/public/hit.mp3")
   );
+  const rigidBodies = useRef(null);
 
+  const cubesCount = 100;
   const cube = useRef();
   const twister = useRef();
+  const cubes = useRef();
+
+  const cubesTransform = useMemo(() => {
+    const positions = [];
+    const rotations = [];
+    const scales = [];
+
+    for (let i = 0; i < cubesCount; i++) {
+      positions.push([i * 2, 0, 0]);
+      rotations.push([0, 0, 0]);
+      scales.push([1, 1, 1]);
+    }
+    return { positions, rotations, scales };
+  }, []);
+
+  const instances = useMemo(() => {
+    const instances = [];
+
+    const scale = 0.2 + Math.random() * 0.8;
+
+    for (let i = 0; i < cubesCount; i++) {
+      instances.push({
+        key: "instance_" + Math.random(),
+        position: [
+          (Math.random() - 0.5) * 8,
+          6 + i * 0.2,
+          (Math.random() - 0.5) * 8,
+        ],
+        rotation: [
+          Math.random(),
+          Math.random(),
+          Math.random(),
+        ],
+        scale: [scale, scale, scale],
+      });
+    }
+
+    return instances;
+  }, []);
+  useEffect(() => {
+    if (!rigidBodies.current) {
+      return;
+    }
+    console.log(rigidBodies);
+    console.log(cubes);
+
+    // You can access individual instanced by their index
+
+    // Or update all instances
+    rigidBodies.current.forEach((api) => {
+      api.applyImpulse({ x: 0, y: 10, z: 0 }, true);
+    });
+  }, [cubes]);
+
+  // useEffect(() => {
+  //   console.log(cubes);
+  //   for (let i = 0; i < cubesCount; i++) {
+  //     // * the instance transformation,position,rotation,scale,unit matrix4
+  //     const matrix = new Matrix4();
+  //     matrix.compose(
+  //       new Vector3(i * 2, 0, 0),
+  //       new Quaternion(),
+  //       new Vector3(1, 1, 1)
+  //     );
+  // * i is the index of the instance
+  //     cubes.current.setMatrixAt(i, matrix);
+  //   }
+  // }, []);
+
   useFrame((state, delta) => {
     // cube.current.rotation.y += delta * 0.2;
     const time = state.clock.elapsedTime;
@@ -59,12 +137,13 @@ export default function PhysicsExperience() {
 
   const collisionEnter = () => {
     console.log("collision");
-    hitSound.currentTime = 0;
-    hitSound.volume = Math.random();
-    hitSound.play();
+    // hitSound.currentTime = 0;
+    // hitSound.volume = Math.random();
+    // hitSound.play();
   };
   return (
     <>
+      <Perf position="top-left"></Perf>
       <OrbitControls makeDefault />
 
       <directionalLight
@@ -76,7 +155,8 @@ export default function PhysicsExperience() {
       <ambientLight intensity={0.5} />
       <Physics gravity={[0, -9.08, 0]}>
         {/* // * showing the colliders wireframe */}
-        <Debug></Debug>
+        {/* // * some times debug will add wireframs lines, bad for perf */}
+        {/* <Debug></Debug> */}
         <RigidBody
           //  * shape of the wrapper
           colliders="ball"
@@ -193,7 +273,24 @@ export default function PhysicsExperience() {
             <meshStandardMaterial color="red" />
           </mesh>
         </RigidBody>
-
+        <InstancedRigidBodies
+          positions={cubesTransform.positions}
+          rotations={cubesTransform.rotations}
+          scales={cubesTransform.scales}
+          // * new api,pass the instances position,rotation,everything
+          instances={instances}
+          ref={rigidBodies}
+        >
+          <instancedMesh
+            ref={cubes}
+            args={[undefined, undefined, cubesCount]}
+            count={cubesCount}
+            castShadow
+          >
+            <boxGeometry></boxGeometry>
+            <meshStandardMaterial color="tomato"></meshStandardMaterial>
+          </instancedMesh>
+        </InstancedRigidBodies>
         <RigidBody
           // * hull will has some bugs
           colliders={false}
@@ -210,6 +307,25 @@ export default function PhysicsExperience() {
             />
           </CylinderCollider>
         </RigidBody>
+        <RigidBody type="fixed">
+          <CuboidCollider
+            args={[5, 2, 0.5]}
+            position={[0, 1, 5.5]}
+          />
+          <CuboidCollider
+            args={[5, 2, 0.5]}
+            position={[0, 1, -5.5]}
+          />
+          <CuboidCollider
+            args={[0.5, 2, 5]}
+            position={[5.5, 1, 0]}
+          />
+          <CuboidCollider
+            args={[0.5, 2, 5]}
+            position={[-5.5, 1, 0]}
+          />
+        </RigidBody>
+        {/*  // * using same geometry and material, geometry, material, numbers of the instance  */}
       </Physics>
     </>
   );
